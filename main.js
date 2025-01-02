4f5782964b7ed2c2076589a52754d0ae713d4b5d
tzeeeeee@gmail.com <tzeeeeee@gmail.com>
2025-01-02 13:12:03 +0900

    강의내용업로드

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import _ from 'lodash';
 
-console.log('hello world!');
-console.log(_.camelCase('hello world'));
+const heropy = {
+  firstName: 'heropy',
+  lastName: 'park',
+  getFullName: function (){
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+console.log(heropy.getFullName());
+
+//
+
+const amy = {
+  firstName:'Amy',
+  lastName:'clarke',
+  getFullName: function(){
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+console.log(amy.getFullName());
